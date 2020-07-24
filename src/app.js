717de099ad77b2331f36d26e00a833e211955622
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2020-07-24 13:15:55 +0500

    error handling

--- src/app.js
+++ src/app.js
@@ -23,6 +23,37 @@ app.use(router);
 mongoose.connect(config.mongodbUrl(), { useNewUrlParser: true, useUnifiedTopology: true })
 
 
+// Error handlers
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+// Development error handler
+// Will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (error, req, res, next) {
+        res.status(error.status || 500);
+        res.send({
+            message: error.message,
+            error: error
+        });
+    });
+}
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function (error, req, res, next) {
+    res.status(error.status || 500);
+    res.send({
+        message: error.message,
+        error: error
+    });
+});
+
 
 app.listen(port, () => console.log(`Server is running on  http://localhost:${port}`));
 
